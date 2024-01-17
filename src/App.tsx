0ac83664084095710a6f0dfb80945cd1dfe2348d
Noalino <bgelineau.dev@gmail.com>
2024-01-17 16:36:34 +0100

    Create search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
+import type { ChangeEvent, FormEvent } from 'react';
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [asset, setAsset] = useState('');
+
+  const handleChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+    setAsset(target.value);
+  };
+
+  const handleOnSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    // Should trim asset string
+    console.log('onSubmit', event);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form onSubmit={handleOnSubmit}>
+      <input
+        id="asset"
+        name="asset"
+        placeholder="Enter a product name or ISIN"
+        value={asset}
+        onChange={handleChange}
+      />
+      <button type="submit" disabled={asset.trim().length === 0}>
+        Search
+      </button>
+    </form>
   );
 }
 
