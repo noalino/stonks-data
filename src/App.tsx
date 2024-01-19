68d2721dca6ad4fa40db641f73a654e0b539bc59
Noalino <bgelineau.dev@gmail.com>
2024-01-19 09:45:24 +0100

    Add basic layout style

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { search } from './api';
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-import './App.css';
 
 const formSchema = z.object({
   searchValue: z.string().trim().min(1),
@@ -39,24 +38,29 @@ function App() {
   }, [watch]);
 
   return (
-    <Form {...form}>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex">
-        <FormField
-          control={control}
-          name="searchValue"
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Input placeholder="Enter a product name or ISIN" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <Button type="submit" disabled={disabled} className="ml-4">
-          Search
-        </Button>
-      </form>
-    </Form>
+    <div className="flex flex-col min-h-screen items-center justify-center">
+      <Form {...form}>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex">
+          <FormField
+            control={control}
+            name="searchValue"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder="Enter a product name or ISIN"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={disabled} className="ml-4">
+            Search
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 }
 
