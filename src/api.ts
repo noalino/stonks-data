2bb901b73324707695176aa7ef2ea17b5ffb9d7b
Noalino <bgelineau.dev@gmail.com>
2024-01-20 10:57:44 +0100

    Add monthly time series request

--- src/api.ts
+++ src/api.ts
@@ -10,18 +10,37 @@ export type SearchMatch = {
   '9. matchScore': string;
 };
 
+export type MonthlyTimeSeries = {
+  string: {
+    '1. open': string;
+    '2. high': string;
+    '3. low': string;
+    '4. close': string;
+    '5. volume': string;
+  };
+};
+
 type SearchResponse = {
   bestMatches: SearchMatch[];
 };
-/* eslint-disable */
+
+type MonthlyTimeSeriesResponse = {
+  'Meta Data': {
+    '1. Information': string;
+    '2. Symbol': string;
+    '3. Last Refreshed': string;
+    '4. Time Zone': string;
+  };
+  'Monthly Time Series': MonthlyTimeSeries;
+};
+
 const API_URL = 'https://www.alphavantage.co/query?';
 
 export async function search(
   value: string
 ): Promise<SearchResponse['bestMatches']> {
   const FUNCTION = 'SYMBOL_SEARCH';
-  // const url = `${API_URL}function=${FUNCTION}&keywords=${value}&apikey=${import.meta.env.VITE_API_KEY}`;
-  const url = 'http://localhost:8000/search-results.json';
+  const url = `${API_URL}function=${FUNCTION}&keywords=${value}&apikey=${import.meta.env.VITE_API_KEY}`;
 
   try {
     const data = await fetch(url);
@@ -30,5 +49,21 @@ export async function search(
   } catch (err) {
     console.log(err);
     return [];
+  }
+}
+
+export async function monthlyTimeSeries(
+  symbol: string
+): Promise<MonthlyTimeSeriesResponse['Monthly Time Series'] | undefined> {
+  const FUNCTION = 'TIME_SERIES_MONTHLY';
+  const url = `${API_URL}function=${FUNCTION}&symbol=${symbol}&apikey=${import.meta.env.VITE_API_KEY}`;
+
+  try {
+    const data = await fetch(url);
+    const results = await data.json();
+    return results['Monthly Time Series'];
+  } catch (err) {
+    console.log(err);
+    return;
   }
 }
